b153454dd3eb72113126d59383b0b8e1baba74cf
dsummers14 <44845612+dsummers14@users.noreply.github.com>
2020-07-31 16:59:31 -0400

    Editing Sales Lines

---
+++ Controllers/SalesOrderLineController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.NAV;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+
+namespace CustomerPortal.Controllers
+{
+    public class SalesOrderLineController : Controller
+    {
+
+
+        public ActionResult SalesOrderLines_Read(DataSourceRequest request, Guid documentId)
+        {
+            IQueryable<salesOrderLine> salesOrderLines = null;
+            Uri iUri = new Uri(ODataWebService.BuildODataUrl());
+            NAV iWebService = new NAV(iUri) { Credentials = ODataWebService.CreateCredentials(iUri.ToString()) };
+
+            try
+            {
+                salesOrderLines = (from lsalesOrderLine in iWebService.salesOrderLines where lsalesOrderLine.documentId == documentId select lsalesOrderLine);
+            }
+            catch (Exception ex)
+            {
+                // return ex;
+            }
+
+            return Json(salesOrderLines.ToDataSourceResult(request));
+        }
+
+        [HttpPost]
+        public ActionResult SalesOrderLine_Update(DataSourceRequest request, salesOrderLine OrderLine)
+        {
+            List<salesOrderLine> iResult = new List<salesOrderLine>();
+
+
+
+
+
+            return Json(iResult.ToDataSourceResult(request, ModelState));
+        }
+        [HttpPost]
+        public ActionResult SalesOrderLine_Create(DataSourceRequest request, salesOrderLine OrderLine)
+        {
+            List<salesOrderLine> iResult = new List<salesOrderLine>();
+
+
+
+
+
+            return Json(iResult.ToDataSourceResult(request, ModelState));
+        }
+       [HttpDelete]
+        public ActionResult SalesOrderLines_Delete(DataSourceRequest request, salesOrderLine OrderLine)
+        {
+            List<salesOrderLine> iResult = new List<salesOrderLine>();
+            Uri iUri = new Uri(ODataWebService.BuildODataUrl());
+            NAV iWebService = new NAV(iUri) { Credentials = ODataWebService.CreateCredentials(iUri.ToString()) };
+
+            try
+            {
+                var salesOrderLine = (from lsalesOrderLine in iWebService.salesOrderLines where lsalesOrderLine.id == OrderLine.id select lsalesOrderLine);
+            }
+            catch (Exception ex)
+            {
+                // return ex;
+            }
+
+            return Json(iResult.ToDataSourceResult(request));
+        }
+
+    }
+}
+
+
+
---
+++ Models/salesOrder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Microsoft.NAV
+{
+    public partial class salesOrder : global::Microsoft.OData.Client.BaseEntityType
+    {
+        public string orderDateString
+        {
+        get
+            {
+              return ODataWebService.EdmDateToDateTime(orderDate).ToShortDateString();
+            }
+        }
+        public string requestedDeliveryDateString
+        {
+            get
+            {
+                return ODataWebService.EdmDateToDateTime(requestedDeliveryDate).ToShortDateString();
+            }
+        }
+        public string postingDateString
+        {
+            get
+            {
+                return ODataWebService.EdmDateToDateTime(postingDate).ToShortDateString();
+            }
+        }
+
+    }
+}
